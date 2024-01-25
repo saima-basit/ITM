99cc318f872eef74c341f55e192ab5f997a7cbd9
saima-basit <saima.ba.18@gmail.com>
2024-01-25 19:04:03 +0500

    Docker changes

--- 3-tier-project/app.py
+++ 3-tier-project/app.py
@@ -41,4 +41,4 @@ def delete_contacts(contact_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=1234)
+    app.run(debug=True, port=1111)
--- 3-tier-project/deploy.sh
+++ 3-tier-project/deploy.sh
@@ -4,3 +4,5 @@ echo "Building docker images"
 docker build -t itm_project .
 echo "Running containers"
 docker-compose up -d server
+
+docker run -itd -p 8888:1111 itm_project
